62810048fcdbf75e7f38816f21a09aee140fa87d
Anshuman Morey <anshumanmorey2@gmail.com>
2026-09-23 18:06:00 +0530

    Add TrackingPage component with styling and routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from './Pages/HomePage'
 import { Routes , Route} from 'react-router';
 import { CheckoutPage} from './Pages/CheckoutPage';
 import { OrdersPage} from './Pages/OrdersPage';
+import { TrackingPage } from './Pages/TrackingPage';
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage />}></Route>
       <Route path="checkout" element={<CheckoutPage />}></Route>
       <Route path='Orders' element={<OrdersPage />}></Route>
+      <Route path='Tracking' element={<TrackingPage />}></Route>
     </Routes>
      
   )
